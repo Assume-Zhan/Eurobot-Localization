5083910c6e271d115b408e2fc1c950cb859785ee
Assume-Zhan <assume@gmail.com>
2023-02-20 17:55:46 +0800

    MODIFY : install script

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 mv ~/Eurobot-Localization ~/Localization2023_ws
-cd ~/Localization2023_ws/src/.YDLidar-SDK/build
+mv ~/Localization2023_ws/src/.YDLidar-SDK ~/Localization2023_ws/src/YDLidar-SDK
+cd ~/Localization2023_ws/src/YDLidar-SDK/build
 cmake ..
 make
 sudo make install
+sudo apt update
 sudo apt install ros-noetic-costmap-converter -y
 sudo apt install ros-noetic-robot-localization -y
 cd ~/Localization2023_ws
 catkin_make
+mv ~/Localization2023_ws/src/YDLidar-SDK ~/Localization2023_ws/src/.YDLidar-SDK
