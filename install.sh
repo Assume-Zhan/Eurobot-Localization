b78473d4a62043ac3d600aa400d8c8f460663e12
Assume-Zhan <assume@gmail.com>
2023-02-20 18:39:57 +0800

    MODIFY : install script

--- install.sh
+++ install.sh
@@ -11,3 +11,9 @@ sudo apt install ros-noetic-robot-localization -y
 cd ~/Localization2023_ws
 catkin_make
 mv ~/Localization2023_ws/src/YDLidar-SDK ~/Localization2023_ws/src/.YDLidar-SDK
+
+echo "source ~/Localization2023_ws/devel/setup.bash" >> ~/.bashrc
+cp ydlidar_ros_driver ~/ws/src/ydlidar_ros_driver
+roscd && cd ..
+chmod 777 src/ydlidar_ros_driver/startup/*
+sudo sh src/ydlidar_ros_driver/startup/initenv.sh
