f25fea598069d85b9d700558b111a57a9bda6c8e
Assume-Zhan <assume@gmail.com>
2023-02-23 13:17:23 +0800

    MODIFY : install shell script

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
+# Install basic package
+sudo apt update
+sudo apt install ros-noetic-costmap-converter -y
+sudo apt install ros-noetic-robot-localization -y
+sudo apt install ros-noetic-imu-tools -y
+
+# Rename the workspace
 WS_PATH="$( find ~ -name Eurobot-Localization )"
 NEW_WS_PATH="${WS_PATH/'Eurobot-Localization'/'Localization2023_ws'}"
 
 mv $WS_PATH $NEW_WS_PATH
+
+# Change visible og lidar sdk
+# Build lidar sdk
 mv $NEW_WS_PATH/src/.YDLidar-SDK $NEW_WS_PATH/src/YDLidar-SDK
 cd $NEW_WS_PATH/src/YDLidar-SDK && mkdir build && cd build
 cmake ..
 make
 sudo make install
-sudo apt update
-sudo apt install ros-noetic-costmap-converter -y
-sudo apt install ros-noetic-robot-localization -y
-sudo apt install ros-noetic-imu-tools -y
+mv $NEW_WS_PATH/src/YDLidar-SDK $NEW_WS_PATH/src/.YDLidar-SDK
+
+# Workspace setup
 cd $NEW_WS_PATH
 catkin_make
-mv $NEW_WS_PATH/src/YDLidar-SDK ~/Localization2023_ws/src/.YDLidar-SDK
-
 echo "source $NEW_WS_PATH/devel/setup.bash" >> ~/.bashrc
-cd $NEW_WS_PATH
 
+# USB driver setup
+cd $NEW_WS_PATH
 chmod 777 src/ydlidar_ros_driver/startup/*
 sudo sh src/ydlidar_ros_driver/startup/initenv.sh
 cd $NEW_WS_PATH/src/phidgets_drivers/phidgets_api
