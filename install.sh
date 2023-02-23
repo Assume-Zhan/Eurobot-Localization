bd24564909879bef4909d655f6b891bc42c49a40
Assume-Zhan <assume@gmail.com>
2023-02-23 13:00:23 +0800

    MODIFY : install shell script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-mv ~/Eurobot-Localization ~/Localization2023_ws
-mv ~/Localization2023_ws/src/.YDLidar-SDK ~/Localization2023_ws/src/YDLidar-SDK
-cd ~/Localization2023_ws/src/YDLidar-SDK && mkdir build && cd build
+WS_PATH="$( find ~ -name Eurobot-Localization )"
+NEW_WS_PATH="${WS_PATH/'Eurobot-Localization'/'Localization2023_ws'}"
+
+mv $WS_PATH $NEW_WS_PATH
+mv $NEW_WS_PATH/src/.YDLidar-SDK $NEW_WS_PATH/src/YDLidar-SDK
+cd $NEW_WS_PATH/src/YDLidar-SDK && mkdir build && cd build
 cmake ..
 make
 sudo make install
@@ -8,15 +11,15 @@ sudo apt update
 sudo apt install ros-noetic-costmap-converter -y
 sudo apt install ros-noetic-robot-localization -y
 sudo apt install ros-noetic-imu-tools -y
-cd ~/Localization2023_ws
+cd $NEW_WS_PATH
 catkin_make
-mv ~/Localization2023_ws/src/YDLidar-SDK ~/Localization2023_ws/src/.YDLidar-SDK
+mv $NEW_WS_PATH/src/YDLidar-SDK ~/Localization2023_ws/src/.YDLidar-SDK
 
-echo "source ~/Localization2023_ws/devel/setup.bash" >> ~/.bashrc
-cd ~/Localization2023_ws
+echo "source $NEW_WS_PATH/devel/setup.bash" >> ~/.bashrc
+cd $NEW_WS_PATH
 
 chmod 777 src/ydlidar_ros_driver/startup/*
 sudo sh src/ydlidar_ros_driver/startup/initenv.sh
-cd ~/Localization2023_ws/src/phidgets_drivers/phidgets_api
+cd $NEW_WS_PATH/src/phidgets_drivers/phidgets_api
 sudo cp debian/udev /etc/udev/rules.d/99-phidgets.rules
 sudo udevadm control --reload-rules
