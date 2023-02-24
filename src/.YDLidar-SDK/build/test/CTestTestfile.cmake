37bf8665e8ffb65fd3f37034378f8eb51d200112
Assume-Zhan <assume@gmail.com>
2023-02-24 18:03:20 +0800

    REMOVE : src

--- src/.YDLidar-SDK/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Localization2023_ws/src/YDLidar-SDK/test
-# Build directory: /home/ubuntu/Localization2023_ws/src/YDLidar-SDK/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(lidar_test "lidar_test")
-set_tests_properties(lidar_test PROPERTIES  _BACKTRACE_TRIPLES "/home/ubuntu/Localization2023_ws/src/YDLidar-SDK/test/CMakeLists.txt;10;add_test;/home/ubuntu/Localization2023_ws/src/YDLidar-SDK/test/CMakeLists.txt;0;")
--- src/eurobot_localization/README.md
+++
@@ -1,1 +0,0 @@
-# eurobot_localization
