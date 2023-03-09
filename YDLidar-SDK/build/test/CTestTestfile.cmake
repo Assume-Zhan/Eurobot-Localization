70775b99e3de52404dcdeaaa4a16d76d636ee4a3
LeoKang0801 <bartkang0818@gmail.com>
2023-03-09 19:47:33 +0800

    REMOVE : ydlidar sdk

--- YDLidar-SDK/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leokang/Eurobot2023_localization_ws/src/localization/YDLidar-SDK/test
-# Build directory: /home/leokang/Eurobot2023_localization_ws/src/localization/YDLidar-SDK/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(lidar_test "lidar_test")
-set_tests_properties(lidar_test PROPERTIES  _BACKTRACE_TRIPLES "/home/leokang/Eurobot2023_localization_ws/src/localization/YDLidar-SDK/test/CMakeLists.txt;10;add_test;/home/leokang/Eurobot2023_localization_ws/src/localization/YDLidar-SDK/test/CMakeLists.txt;0;")
--- YDLidar-SDK/doc/FAQs/Hardware_FAQs.md
+++
@@ -1,8 +0,0 @@
-# Hardware FAQs
-
----
-### Which types of YD LiDAR are supported by YDLIDAR-SDK?
-please visit [this](https://github.com/YDLIDAR/YDLidar-SDK/blob/master/doc/Dataset.md) page.
-
----
-**More Hardware FAQs to follow.**
