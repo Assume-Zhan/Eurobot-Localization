46d9a6e2ec3042b33df8b5bcda3e7b9951c594fa
Assume-Zhan <assume@gmail.com>
2023-04-02 03:34:59 +0800

    MODIFY : install shell script

--- install.sh
+++ install.sh
@@ -1,32 +1,45 @@
 # Install basic package
 sudo apt update -y
-sudo apt install ros-noetic-costmap-converter -y
-sudo apt install ros-noetic-robot-localization -y
-sudo apt install ros-noetic-imu-tools -y
+sudo apt install ros-noetic-costmap-converter \
+                 ros-noetic-robot-localization \
+                 ros-noetic-imu-tools \
+                 libusb-1.0-0 libusb-1.0-0-dev -y
 
 # Rename the workspace
-WS_PATH="$( find ~ -name Eurobot-Localization )"
-NEW_WS_PATH="${WS_PATH/'Eurobot-Localization'/'localization'}"
+WS_PATH="$( find ~ -name Eurobot-Localization | awk '{print $1}' | head -1)"
 
-mv $WS_PATH $NEW_WS_PATH
-
-# Change visible og lidar sdk
+# Change visible of lidar sdk
 # Build lidar sdk
-mv $NEW_WS_PATH/.YDLidar-SDK $NEW_WS_PATH/YDLidar-SDK
-cd $NEW_WS_PATH/YDLidar-SDK && mkdir build && cd build
+mv $WS_PATH/.YDLidar-SDK $WS_PATH/YDLidar-SDK
+cd $WS_PATH/YDLidar-SDK && mkdir build && cd build
 cmake ..
 make
 sudo make install
 # echo "Done make"
-mv $NEW_WS_PATH/YDLidar-SDK $NEW_WS_PATH/.YDLidar-SDK
+mv $WS_PATH/YDLidar-SDK $WS_PATH/.YDLidar-SDK
 
-# Workspace setup
-cd $NEW_WS_PATH/../..
+# Go back to Workspace
+check_backto_ws(){
+    if [ $(ls | grep devel | wc -l) = 1 ]; then
+        echo "BACK TO WS"
+        return 0
+    else 
+      
+        cd ..
+        
+        if [ $(pwd) = "/home" ] || [ $(pwd) = "/" ]; then
+            return -1
+        else
+            check_backto_ws
+        fi
+
+    fi
+}
+check_backto_ws()
 catkin_make
-# echo "Done catkin_make"
 
 # USB driver setup
-cd $NEW_WS_PATH
-cd $NEW_WS_PATH/local_filter/imu/phidgets_drivers/phidgets_api
+cd $WS_PATH
+cd $WS_PATH/local_filter/imu/phidgets_drivers/phidgets_api
 sudo cp debian/udev /etc/udev/rules.d/99-phidgets.rules
 sudo udevadm control --reload-rules
