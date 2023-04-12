3f53cfd2af21fb5494a6e781b3a4e8070605cb8e
Assume-Zhan <assume0701@gmail.com>
2023-04-13 03:04:49 +0800

    ADD : obstacle stamp

--- eurobot_localization/src/rival_localization.cpp
+++ eurobot_localization/src/rival_localization.cpp
@@ -259,6 +259,7 @@ bool RivalMulti::distribute_rival_odom(bool rival1_ok, bool rival2_ok, std::vect
     // ADD : publish other obstacle data to lidar obstacle topic
     obstacle_detector::Obstacles output_obstacles;
     output_obstacles.header.frame_id = "robot1/map";
+    output_obstacles.header.stamp = ros::Time::now();
     for(auto obstacle : Lidar_vec){
         obstacle_detector::CircleObstacle circle;
         circle.center.x = obstacle.x;
